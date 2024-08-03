dddd7d5e423d8cb551bb3af8d588377d105ca752
chaeyeongLEE <dh503401@naver.com>
2024-08-03 15:07:33 +0900

    useState 상태값 변경하기

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,8 +1,9 @@
-import reactImg from './assets/react-core-concepts.png'
-import componentsImg from './assets/components.png';
-import {CORE_CONCEPTS} from './data.js';
-import Header from "./components/Header.jsx";
-import CoreConcept from "./components/CoreConcept.jsx";
+import { useState } from 'react';
+import { CORE_CONCEPTS } from './data.js';
+import Header from './components/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton';
+import { EXAMPLES } from './data.js';
 
 // function CoreConcept(props) {
 //     return (
@@ -15,22 +16,55 @@ import CoreConcept from "./components/CoreConcept.jsx";
 // }
 
 function App() {
-    return (
-        <div>
-            <Header />
-            <main>
-                <section id="core-concepts">
-                    <h2>Core Concepts</h2>
-                    <ul>
-                        <CoreConcept title={CORE_CONCEPTS[0].title} image={CORE_CONCEPTS[0].image} description={CORE_CONCEPTS[0].description} />
-                        <CoreConcept title={CORE_CONCEPTS[1].title} image={CORE_CONCEPTS[1].image} description={CORE_CONCEPTS[1].description} />
-                        <CoreConcept {...CORE_CONCEPTS[2]} />
-                        <CoreConcept {...CORE_CONCEPTS[3]} />
-                    </ul>
-                </section>
-            </main>
-        </div>
-    );
+  const [selectedTopic, setSelectedTopic] = useState('components'); //내부함수에 중첩되면안되며, 컴포넌트 함수의 최상위에서 호출해야한다.
+
+  function handleSelect(selectButton) {
+    setSelectedTopic(selectButton);
+    // console.log(selectedTopic);
+  }
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              image={CORE_CONCEPTS[0].image}
+              description={CORE_CONCEPTS[0].description}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[1].title}
+              image={CORE_CONCEPTS[1].image}
+              description={CORE_CONCEPTS[1].description}
+            />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>
+              component
+            </TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export default App;
