102b0ae6fbd7ba4a1a9895c6d32a120465b04995
chaeyeongLEE <dh503401@naver.com>
2024-08-06 22:37:46 +0900

    투자계산기 프로젝트 개발완료

--- code/05 Essentials Practice/01-starting-project/src/components/Header.jsx
+++ code/05 Essentials Practice/01-starting-project/src/components/Header.jsx
@@ -2,9 +2,9 @@ import headerLogo from '../assets/investment-calculator-logo.png'
 
 export default function Header({message}) {
     return (
-        <div id="header">
+        <header id="header">
             <img src={headerLogo} alt="logoImg" />
             <h1>{message}</h1>
-        </div>
+        </header>
     )
 }
