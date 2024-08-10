334011f7571e1ca3b958dc863397a35948dbec3b
chaeyeongLEE <dh503401@naver.com>
2024-08-10 15:31:48 +0900

    모달창으로 props값 전달하기

--- code/08 Refs Portals/01-starting-project/src/components/ResultModal.jsx
+++ code/08 Refs Portals/01-starting-project/src/components/ResultModal.jsx
@@ -1,13 +1,18 @@
 import {forwardRef} from "react";
 
 const ResultModal = forwardRef(
-    function ResultModal({result, targetTime}, ref) {
+    function ResultModal({targetTime, remainingTime, onReset}, ref) {
+
+       const userLost = remainingTime <= 0;
+       const formattedRemainingTime = (remainingTime/1000).toFixed(2);
+       const score = Math.round((1-remainingTime/(targetTime * 1000)) * 100);
         return (
             <dialog ref={ref} className="result-modal">
-                <h2>you {result}</h2>
+                {userLost && <h2>you lost</h2>}
+                {!userLost && <p>점수:{score} 점 입니다.</p>}
                 <p>목표시간은 <strong>{targetTime}</strong>입니다.</p>
-                <p>잔여시간은 <strong>X</strong>입니다.</p>
-                <form method="dialog">
+                <p>잔여시간은 <strong>{formattedRemainingTime}</strong>초 입니다.</p>
+                <form method="dialog" onSubmit={onReset}>
                     <button>Close</button>
                 </form>
             </dialog>
